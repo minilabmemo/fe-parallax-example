a3cc3b41ea8275a74d284c0ea4365cae0422f7fc
minilabmemo <mini.lab.memo@gmail.com>
2023-04-09 14:13:50 +0800

    Fix: click tidy

--- src/js/click.js
+++ src/js/click.js
@@ -4,9 +4,5 @@ scolldown = function () {
   console.log("test")
   console.log(`document.body.clientHeight ${document.body.scrollHeight}`)
   console.log(`document.body.scrollHeight ${document.body.clientHeight}`)
-  window.scrollTo(0, document.body.scrollHeight)
+  window.scrollTo(0, document.body.clientHeight)
 }
-
-// function scolldown() {
-//   console.log("test")
-// }
