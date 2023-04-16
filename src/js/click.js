ac0c21a946065cf504b4c52210182dd2cc158ae1
minilabmemo <mini.lab.memo@gmail.com>
2023-04-16 10:06:18 +0800

    Fix: layout next page half

--- src/js/click.js
+++ src/js/click.js
@@ -1,8 +1,5 @@
 
 
 scolldown = function () {
-  console.log("test")
-  console.log(`document.body.clientHeight ${document.body.scrollHeight}`)
-  console.log(`document.body.scrollHeight ${document.body.clientHeight}`)
   window.scrollTo(0, document.body.clientHeight)
 }
