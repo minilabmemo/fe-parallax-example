a905b4e184b9e7dcac1be5f8a7b93e9b13d47c03
minilabmemo <minilabmemo@gmail.com>
2023-08-01 11:45:10 +0800

    Fix: scss files tidy

---  __tests__/styleCss.test.js
+++  __tests__/styleCss.test.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
-test("測試 main.css 快照", () => {
-  // 讀取 main.css 檔案的內容
-  const cssPath = path.join(__dirname, "../src/css/main.css");
+test("測試 style.css 快照", () => {
+  // 讀取 css 檔案的內容
+  const cssPath = path.join(__dirname, "../src/css/style.css");
   const cssContent = fs.readFileSync(cssPath, "utf8");
 
   // 將內容與快照進行比較
